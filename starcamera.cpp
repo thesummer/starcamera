c139827f8473d1066cd70d6420d0699f6f473f8b
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2013-06-06 14:29:25 -0600

    Write function to load image frame from file

--- starcamera.cpp
+++ starcamera.cpp
@@ -1,3 +1,5 @@
+#include <fstream>
+
 #include "starcamera.h"
 
 StarCamera::StarCamera()
@@ -10,9 +12,32 @@ void StarCamera::getImage()
     /// TODO: grap a frame from Aptina using Apbase library
 }
 
-void StarCamera::getImageFromFile(const char* filename)
+void StarCamera::getImageFromFile(const char* filename, int rows, int cols)
 {
-    /// TODO: load an image from file
+    // open image file
+    std::fstream file;
+    file.open(filename, ios_base::in | ios_base::binary);
+    if(!file.is_open())
+    {
+        /// TODO throw error
+    }
+
+    // resize matrix structure for image data if necessary
+    mFrame.create(rows, cols);
+
+    // read image data and transform from 12 to 8 bit
+    for (int i = 0; i<rows; ++i)
+    {
+        for(int j = 0; j<cols; ++j)
+        {
+            u_int16_t temp;
+            file.read((char*) &temp, sizeof(u_int16_t) );
+
+            mFrame(i,j) = temp/16;
+        }
+    }
+
+    file.close();
 }
 
 int StarCamera::extractSpots()
--- starcamera.h
+++ starcamera.h
@@ -1,6 +1,9 @@
 #ifndef STARCAMERA_H
 #define STARCAMERA_H
 
+#include <opencv2/core/core.hpp>
+#include <stdint.h>
+
 class StarCamera
 {
 public:
@@ -8,11 +11,13 @@ public:
 
     void getImage();
 
-    void getImageFromFile(const char *filename);
+    void getImageFromFile(const char *filename, int rows, int cols);
 
     int extractSpots();
 
     void calculateSpotVectors();
+
+    cv::Mat_<u_int8_t> mFrame;
 };
 
 #endif // STARCAMERA_H
