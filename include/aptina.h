b4407d392a1f93eb1ad79c3c4b8d20ef976091be
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2013-07-26 13:44:33 -0600

    Add class for aptina camera

---
+++ include/aptina.h
@@ -0,0 +1,32 @@
+#ifndef APTINA_H
+#define APTINA_H
+
+#include <stdint.h>
+
+#include "apbase.h"
+
+class Aptina
+{
+public:
+    Aptina();
+    ~Aptina();
+
+    void initialize();
+    bool grabFrame(uint8_t **imageBuf);
+
+
+
+
+    ap_u32 getWidth() const { return mWidth; }
+    ap_u32 getHeight() const { return mHeight; }
+    ap_u32 getBufferSize() const { return mBufferSize; }
+
+private:
+    AP_HANDLE mHandle;
+    ap_u32 mWidth;
+    ap_u32 mHeight;
+    ap_u32 mBufferSize;
+    uint8_t * mImageBuf;
+};
+
+#endif
---
+++ src/aptina.cpp
@@ -0,0 +1,53 @@
+#include <stdexcept>
+#include <iostream>
+using std::endl;
+using std::cout;
+
+#include "aptina.h"
+
+Aptina::Aptina()
+    :mHandle(NULL), mWidth(0), mHeight(0)
+{
+}
+
+Aptina::~Aptina()
+{
+    delete [] mImageBuf;
+    ap_Finalize();
+}
+
+
+void Aptina::initialize()
+{
+    if(ap_DeviceProbe(NULL) != AP_CAMERA_SUCCESS)
+        throw std::runtime_error("Unable to either detect a sensor or find a matching SDAT file.");
+
+    mHandle = ap_Create(0);
+    if(mHandle == NULL)
+        throw std::runtime_error("Camera initialization failed");
+    int result = ap_LoadIniPreset(mHandle, NULL, NULL);
+    if( result != AP_INI_SUCCESS)
+        throw std::runtime_error("Failed to load IniPreset");
+
+    ap_GetImageFormat(mHandle, &mWidth, &mHeight, NULL, 0);
+
+    mBufferSize = ap_GrabFrame(mHandle, NULL, 0);
+
+    mImageBuf = new uint8_t[mBufferSize];
+
+    ap_GrabFrame(mHandle, mImageBuf, mBufferSize);
+}
+
+bool Aptina::grabFrame(uint8_t **imageBuf)
+{
+    if(mHandle == NULL)
+        throw std::runtime_error("Grab frame failed. Camera handle not initialized");
+
+    ap_u32 numBytes = ap_GrabFrame(mHandle, mImageBuf, mBufferSize);
+    ap_s32 result = ap_GetLastError();
+    if(result != AP_CAMERA_SUCCESS)
+        throw std::runtime_error("Failed to grab frame from camera");
+
+    *imageBuf = mImageBuf;
+    return true;
+}
